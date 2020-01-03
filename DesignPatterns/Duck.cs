e4469c8bad4b26ebeb1678384a2fd2ca4e1fdd10
Omar Negron Montero <omarnegron92@hotmail.com>
2020-01-02 23:37:19 -0500

    Initial commit

---
+++ DesignPatterns/Duck.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public abstract class Duck
+    {
+        protected IFlyBehavior flyBehavior;
+        protected IQuackBehavior quackBehavior;
+
+        public Duck()
+        {
+
+        }
+
+        public abstract void display();
+
+        public void performFly()
+        {
+            flyBehavior.fly();
+        }
+
+        public void performQuack()
+        {
+            quackBehavior.quack();
+        }
+
+        // setting behavior dynamically
+        public void setFlyBehavior(IFlyBehavior fb)
+        {
+            flyBehavior = fb;
+        }
+
+        public void setQuackBehavior(IQuackBehavior qb)
+        {
+            quackBehavior = qb;
+        }
+
+        public void swim()
+        {
+            Console.WriteLine("All ducks float, even decoys!");
+        }
+    }
+}
---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck mallard = new MallardDuck();
+
+            mallard.performQuack();
+            mallard.performFly();
+
+            Duck model = new ModelDuck();
+
+            model.performFly();
+
+            model.setFlyBehavior(new FlyRocketPowered());
+
+            model.performFly();
+
+            Console.Read();
+        }
+    }
+}
