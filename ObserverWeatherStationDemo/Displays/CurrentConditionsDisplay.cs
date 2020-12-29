dc253e73df7d9fd097a6e784f0d70331e29c56c6
Omar Negron <omarnegron92@hotmail.com>
2020-12-29 15:43:03 -0500

    observer weather station demo

---
+++ ObserverWeatherStationDemo/Displays/CurrentConditionsDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ObserverWeatherStationDemo.Displays
+{
+    public class CurrentConditionsDisplay : IObserver, IDisplayElement
+    {
+        private float temperature;
+        private float humidity;
+        private ISubject weatherData;
+
+        public CurrentConditionsDisplay(ISubject _weatherData)
+        {
+            weatherData = _weatherData;
+            weatherData.registerObserver(this);
+        }
+
+        public void display()
+        {
+            Console.WriteLine("Current conditions: "  + temperature
+                + "F degrees and " + humidity + "% humidity");
+        }
+
+        public void update(float temp, float humidity, float pressure)
+        {
+            this.temperature = temp;
+            this.humidity = humidity;
+            display();
+        }
+    }
+}
