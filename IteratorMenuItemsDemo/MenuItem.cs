67e762b2aedfa695e301bfff4216c61e8f0fb4b6
Omar Negron <omarnegron92@hotmail.com>
2021-03-12 00:25:57 -0500

    iterator menu demo

---
+++ IteratorMenuItemsDemo/MenuItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorMenuItemsDemo
+{
+    public class MenuItem
+    {
+        string name;
+        string description;
+        bool vegetarian;
+        double price;
+
+        public MenuItem(string name, string description, bool vegetarian, double price)
+        {
+            this.name = name;
+            this.description = description;
+            this.price = price;
+            this.vegetarian = vegetarian;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getDescription()
+        {
+            return description;
+        }
+
+        public double getPrice()
+        {
+            return price;
+        }
+
+        public bool isVegetarian()
+        {
+            return vegetarian;
+        }
+    }
+}
---
+++ IteratorMenuItemsDemo/Menus/PancakeHouseMenu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorMenuItemsDemo.Menus
+{
+    public class PancakeHouseMenu
+    {
+        List<MenuItem> menuItems;
+
+        public PancakeHouseMenu()
+        {
+            menuItems = new List<MenuItem>();
+
+            addItem("K&B's Pancake Breakfast", "Pancakes with scrambled eggs, and toast", true, 2.99);
+            addItem("Regular Pancake Breakfast", "Pancakes with fried eggs, sausage", false, 2.99);
+            addItem("Blueberry Pancakes", "Pancakes made with fresh blueberries", true, 3.49);
+            addItem("Waffles", "Waffles, with your choice of blueberries or strawberries", true, 3.59);
+        }
+        
+        public void addItem(string name, string description, bool vegetarian, double price)
+        {
+            MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+            menuItems.Add(menuItem);
+        }
+    }
+}
