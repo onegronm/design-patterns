d6c774ef2302c4edb0da8e4600f4980d76722870
Omar Negron <omarnegron92@hotmail.com>
2021-06-24 00:14:16 -0400

    renamed projects

--- AbstractFactoryPizzaStoreDemo/Pizzas/SimplePizzaFactory.cs
+++ AbstractFactoryPizzaStoreDemo/Pizzas/SimplePizzaFactory.cs
@@ -22,21 +22,21 @@ namespace FactoryPizzaStoreDemo
         {
             IPizza pizza = null;
 
-            switch (type)
-            {
-                case "cheese":
-                    pizza = new CheesePizza();
-                    break;
-                case "pepperoni":
-                    pizza = new PepperoniPizza();
-                    break;
-                case "clam":
-                    pizza = new ClamPizza();
-                    break;
-                case "veggie":
-                    pizza = new VeggiePizza();
-                    break;
-            }
+            //switch (type)
+            //{
+            //    case "cheese":
+            //        pizza = new CheesePizza();
+            //        break;
+            //    case "pepperoni":
+            //        pizza = new PepperoniPizza();
+            //        break;
+            //    case "clam":
+            //        pizza = new ClamPizza();
+            //        break;
+            //    case "veggie":
+            //        pizza = new VeggiePizza();
+            //        break;
+            //}
 
             return pizza;
         }
