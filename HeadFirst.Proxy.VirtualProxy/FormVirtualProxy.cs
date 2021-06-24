2b7511ca11b1ae87a8363e5a2b0b6d0eea60586d
Omar Negron <omarnegron92@hotmail.com>
2021-06-24 00:17:32 -0400

    added form virtual proxy

---
+++ HeadFirst.Proxy.VirtualProxy/FormVirtualProxy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeadFirst.Proxy.VirtualProxy
+{
+    public partial class FormVirtualProxy : Form
+    {
+        public FormVirtualProxy()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HeadFirst.Proxy.VirtualProxy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFirst.Proxy.VirtualProxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
